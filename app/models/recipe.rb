fb46b27b9ca7e9045622d9e272464781d09f40d6
Henrique <hliviero@gmail.com>
2016-02-21 12:54:35 -0300

    Ready to Submission 0 errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,14 +1,14 @@
 class Recipe
   include HTTParty
 
-  key_value = ENV["FOOD2FORK_KEY"] = "9967e3b420584cbed1b73d16b0e5346d"
-  hostport = ENV["FOOD2FORK_SERVER_AND_PORT"] || "www.food2fork.com"
+  key_value = ENV['FOOD2FORK_KEY']
+  hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
   base_uri "http://#{hostport}/api"
-  default_params key: key_value
+  default_params fields: "count,recipes", key: key_value
   format :json
 
   def self.for (keyword)
-    get("/search", query: {query: keyword})["recipes"]
+    get("/search", query: {q: keyword})["recipes"]
   end
 
 end
