9c6b0642c8d33b1c82fb9f684b87f9b47ef7f81c
Henrique <hliviero@gmail.com>
2016-02-21 20:55:45 -0300

    Fix API issue

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe
   include HTTParty
 
-  key_value = ENV['FOOD2FORK_KEY']
+  key_value = ENV['FOOD2FORK_KEY'] = "9967e3b420584cbed1b73d16b0e5346d"
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
   base_uri "http://#{hostport}/api"
   default_params fields: "count,recipes", key: key_value
